6473f6f5ab31d1d99a655dd49f50d9d94447580d
kawsar07 <kawsar.ali.cse@gmail.com>
2021-12-08 16:18:43 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/httppre1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.httppre1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secound_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SecoundPage extends StatelessWidget {
+
+ // const SecoundPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
